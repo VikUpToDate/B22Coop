6c46767d2226413d72d02e38b2ab59c3728354aa
Vik the Big <vik350061@gmail.com>
2024-08-27 00:23:16 -0500

    Cucumber reports added

---
+++ src/test/java/com/AutoExercise/TestCase1/runners/Runner.java
@@ -0,0 +1,30 @@
+package com.AutoExercise.TestCase1.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import utils.DriverHelper;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "src/test/resources/automationExercise/TestCase1", // path to feature file/cucumber file
+        glue = "com/AutoExercise/TestCase1/stepDefinitions", // path to stepDef
+        dryRun = false, // if this is set to true, it will generate snippets if step definition is not defined yet
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class Runner {
+    @AfterClass
+    public static void openReport() {
+        // Opening the report
+        WebDriver driver = DriverHelper.getDriver();
+        driver.get("C:\\Everything\\TeamProject\\target\\cucumber-reports.html");
+    }
+}
